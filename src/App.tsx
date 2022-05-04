c107705d2b228a644666fba9e559c2814c94112a
borgrabbit <ggulto2@gmail.com>
2022-05-04 12:53:43 +0900

    initial setting - wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ko from './lang/ko.json'
 import './App.css';
 
-function App() {
+const queryClient = new QueryClient();
+const locale = navigator.language;
+
+const App : React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale={locale} messages={ko} defaultLocale="en">
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <BrowserRouter>
+            <div className="App">Learn React1</div>
+            <FormattedMessage id="1" defaultMessage="Today is {ts, date, ::yyyyMMdd}" values={{ts: Date.now()}} />
+          </BrowserRouter>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </IntlProvider>
   );
 }
 
